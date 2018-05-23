2922d92a7b6e4058d227fd7fab62dcdcfbb7955d
Simon Hofmann <mail@simon-hofmann.org>
2018-05-24 00:28:28 +0200

    Added GreetingController implementation

---
+++ src/main/kotlin/de/consol/hackaburg/controller/GreetingController.kt
@@ -0,0 +1,14 @@
+package de.consol.hackaburg.controllers
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class GreetingController {
+
+    @GetMapping("greet")
+    fun greet(@RequestParam name: String?): String {
+        return "Hello ${name ?: "world"}!"
+    }
+}
