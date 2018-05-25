6fd3fdf6866a86c9247b6aa516e28289acb58289
Simon Hofmann <simon.hofmann@consol.de>
2018-05-25 13:09:32 +0200

    Added errorhandler

--- src/main/kotlin/de/consol/hackaburg/controller/JokeController.kt
+++ src/main/kotlin/de/consol/hackaburg/controller/JokeController.kt
@@ -1,9 +1,14 @@
 package de.consol.hackaburg.controller
 
 import de.consol.hackaburg.dto.ApiResponse
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.client.ResourceAccessException
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.client.getForObject
 
@@ -11,6 +16,8 @@ import org.springframework.web.client.getForObject
 class JokeController {
     @Value("\${hackaburg.demo.backend}")
     lateinit var baseUrl: String
+
+    private val log = LoggerFactory.getLogger(JokeController::class.java)
 
     @GetMapping("/joke")
     fun getRandomJoke(): String {
@@ -24,5 +31,11 @@ class JokeController {
                 "Germans don't laugh..."
             }
         }
+    }
+
+    @ExceptionHandler(value = [ResourceAccessException::class])
+    fun handleNetworkException(rae: ResourceAccessException): ResponseEntity<String> {
+        log.error("Unable to access backend: ${rae.message}")
+        return ResponseEntity("Unable to access backend.", HttpStatus.BAD_GATEWAY)
     }
 }
